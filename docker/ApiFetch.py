f34147077381a128eca7a60903a222ff0eb96558
mirrolepth <sevketkarabulut@outlook.com.tr>
2023-12-28 22:35:29 +0300

    .

---
+++ docker/ApiFetch.py
@@ -0,0 +1,18 @@
+# Şevket KARABULUT  02205076038
+
+import requests
+
+url = "https://api.n.exchange/en/api/v1/price/BTCUSD/latest/?market_code=nex"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    
+    data = response.json()
+    
+    bid_value = data[0]["ticker"]["bid"]
+
+    
+    print("1 BTC = ", bid_value)
+else:
+    print("Error:", response.status_code)
